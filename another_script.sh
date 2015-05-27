58e56b31b796658326b4a8fbc1e945cfefbd159b
Dave Rivken <drivken@quinn-minors-macbook.local>
2015-05-27 09:55:36 -0400

    Add another scrip

---
+++ another_script.sh
@@ -0,0 +1,30 @@
+# Create one file
+
+for i in {1..100000}
+
+do
+    if [$1==5] then; 
+	touch file$i.txt
+fi
+
+done
+
+echo $i 
+
+
+
+
+
+# Put some centents into file.
+
+
+for j in {1..10};
+
+do
+    if [$j ==5] then
+	echo "Contenets of file $j" > file$j.txt
+    fi
+
+done
+
+echo $j
